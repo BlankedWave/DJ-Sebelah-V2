b4d52304a3303d2b7641a4794e6722915cef3b9d
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-15 22:37:02 +0800

    Update volume.js

--- musicCommands/volume.js
+++ musicCommands/volume.js
@@ -1,36 +1,35 @@
-
 const { EmbedBuilder } = require('discord.js');
 const { getPlayer } = require('./play');
 
 module.exports = {
   name: 'volume',
-  description: 'Adjust the volume of the bot',
+  description: 'Tetapkan volume bot yang sesuai',
   execute: async (message, args) => {
     const volume = parseFloat(args[0]);
 
     if (isNaN(volume) || volume < 0 || volume > 100) {
-      return message.reply('❌ Please provide a valid volume level between 0 and 100.');
+      return message.reply('❌ Sila berikan tahap volume yang sah antara 0 dan 100.');
     }
     const player = getPlayer();
 
     if (!player) {
-      return message.reply('❌ No music is currently playing.');
+      return message.reply('❌ Tiada muzik sedang diputarkan pada masa ini.');
     }
     const resource = player.state.resource;
 
     if (!resource) {
-      return message.reply('❌ No audio resource found.');
+      return message.reply('❌ Tiada sumber audio dijumpai.');
     }
     resource.volume.setVolume(volume / 100);
 
     const embed = new EmbedBuilder()
       .setColor('#2b71ec')
      .setAuthor({
-          name: 'Volume Control!',
+          name: 'Kawalan Volume!',
           iconURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488721546645624/volume.png?ex=656b6a2e&is=6558f52e&hm=8215d2f88ab073db1f3b6438c28fd73315ad7e581bb54000dbb06fca387cecf7&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/X6RT5VdJPQ'
         })
-      .setDescription(`**volume engaged to ${volume}%**`);
+      .setDescription(`**volume diset kepada ${volume}%**`);
 
     message.reply({ embeds: [embed] });
   },
