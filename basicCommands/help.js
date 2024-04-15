53893751a1cf6205d7c82036ac7db362642e1972
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-15 23:09:13 +0800

    Update help.js
    fix error setwidth()

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -16,48 +16,48 @@ module.exports = {
         iconURL: 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 
         url: 'https://discordapp.com/users/267958390389604362'
     })
-    .setWidth(600) // Set the width to 600 pixels
-    .setDescription(`__**STATS :**__\n\n> **📊 Bilangan server yang guna khidmat DJ Sebelah:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Dibina Oleh [BlankedWave](https://discordapp.com/users/267958390389604362)**\n\n__**COMMANDS :**__ `)
-    .addFields(
-      // Basic commands category
-      {
-        name: '▶️  Asas',
-        value: '`avatar`, `owner`, `support`, `invite`, `userinfo`',
-        inline: true,
-      },
-      // Music commands category
-      {
-        name: '▶️  Muzik',
-        value: '`play`, `history`,`volume`,`pause`,`resume`,`247`',
-        inline: true,
-      },
-      //fun category
-      {
-        name: '▶️  Hiburan',
-        value: '`joke`, `meme`, `roll`',
-        inline: true,
-      },
-      //image category
-      {
-        name: '▶️  Gambar',
-        value: '`cat`, `dog`',
-        inline: true,
-      },
-      //anime category
-      {
-        name: '▶️  Anime',
-        value: '`-animecommands untuk maklumat lanjut`',
-        inline: true,
-      },
-      // Utility commands category
-      {
-        name: '▶️  Utility',
-        value: '`kick`, `ban`, `serverinfo`,`userinfo`, `clear`',
-        inline: true,
-      }
-    )
-    .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-    .setImage(`https://media.tenor.com/WDwFEWQxdFAAAAAi/fish.gif`);
+     
+      .setDescription(`__**STATS :**__\n\n> **📊 Bilangan server:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Dibina Oleh [BlankedWave](https://discordapp.com/users/267958390389604362)**\n\n__**COMMANDS :**__ `)
+      .addFields(
+        // Basic commands category
+        {
+          name: '▶️  Asas',
+          value: '`avatar`, `owner`, `support`, `invite`, `userinfo`',
+          inline: true,
+        },
+        // Music commands category
+        {
+          name: '▶️  Muzik',
+          value: '`play`, `history`,`volume`,`pause`,`resume`,`247`',
+          inline: true,
+        },
+        //fun category
+        {
+          name: '▶️  Hiburan',
+          value: '`joke`, `meme`, `roll`',
+          inline: true,
+        },
+        //image category
+        {
+          name: '▶️  Gambar',
+          value: '`cat`, `dog`',
+          inline: true,
+        },
+        //anime category
+        {
+          name: '▶️  Anime',
+          value: '`-animecommands untuk maklumat lanjut`',
+          inline: true,
+        },
+        // Utility commands category
+        {
+          name: '▶️  Utility',
+          value: '`kick`, `ban`, `serverinfo`,`userinfo`, `clear`',
+          inline: true,
+        }
+      )
+      .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
+      .setImage(`https://media.tenor.com/WDwFEWQxdFAAAAAi/fish.gif`);
 
     const button1 = new ButtonBuilder()
       .setLabel('Discord')
