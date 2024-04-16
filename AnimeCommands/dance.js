570d2aff594797cc55b5de83be051c828127aaca
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-16 15:57:58 +0800

    Update dance.js

--- AnimeCommands/dance.js
+++ AnimeCommands/dance.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'dance',
-  description: 'Dance like nobody\'s watching!',
+  description: 'Joget angan tak sudah!',
   async execute(message, args) {
     const sender = message.author;
     const danceGif = await anime.dance();
 
     const embed = new EmbedBuilder()
       .setColor('#ffcc00')
-      .setDescription(`${sender} is dancing! 💃🕺`)
+      .setDescription(`${sender} sedang berjoget! 💃🕺`)
       .setImage(danceGif);
 
     message.reply({ embeds: [embed] });
