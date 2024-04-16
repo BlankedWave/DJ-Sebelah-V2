187dd0b26c9393833dfa1271ad133d0e27e68678
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-16 16:27:04 +0800

    Update scream.js

--- AnimeCommands/scream.js
+++ AnimeCommands/scream.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'scream',
-  description: 'Scream in fear or excitement!',
+  description: 'Menjerit teruja atau ketakutan!',
   async execute(message, args) {
     const sender = message.author;
     const screamGif = await anime.scream();
 
     const embed = new EmbedBuilder()
       .setColor('#ff3300')
-      .setDescription(`${sender} is screaming... 😱`)
+      .setDescription(`${sender} tengah menjerit... 😱`)
       .setImage(screamGif);
 
     message.reply({ embeds: [embed] });
