8cd8512fd964d50e18eae8ee558b4a6bd2b69330
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-16 15:38:32 +0800

    Update blush.js

--- AnimeCommands/blush.js
+++ AnimeCommands/blush.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'blush',
-  description: 'Blush in embarrassment!',
+  description: 'Malu la sangat!',
   async execute(message, args) {
     const sender = message.author;
     const blushGif = await anime.blush();
 
     const embed = new EmbedBuilder()
       .setColor('#ff66b2')
-      .setDescription(`${sender} is blushing... 💖`)
+      .setDescription(`${sender} tengah blushing... 💖`)
       .setImage(blushGif);
 
     message.reply({ embeds: [embed] });
