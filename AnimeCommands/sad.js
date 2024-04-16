68093434bed7fb96d4b8abebfb4d480d04a132ac
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-16 16:24:17 +0800

    Update sad.js

--- AnimeCommands/sad.js
+++ AnimeCommands/sad.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 
 module.exports = {
   name: 'sad',
-  description: 'Express sadness with a GIF!',
+  description: 'Menunjukkan anda tengah bersedih!',
   async execute(message, args) {
     const sender = message.author;
     const sadGif = await anime.sad();
 
     const embed = new EmbedBuilder()
       .setColor('#3366ff')
-      .setDescription(`${sender} is feeling sad... 😢`)
+      .setDescription(`${sender} tengah bersedih... 😢`)
       .setImage(sadGif);
 
     message.reply({ embeds: [embed] });
