599c0415dcc3e44011ac70c6268d9abc38cf425a
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-16 15:52:10 +0800

    Update cry.js

--- AnimeCommands/cry.js
+++ AnimeCommands/cry.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'cry',
-  description: 'Send a crying gif in an embedded message!',
+  description: 'Menunjukkan yang anda sedang menangis!',
   async execute(message, args) {
     const sender = message.author;
     const cryGif = await anime.cry();
 
     const embed = new EmbedBuilder()
       .setColor('#0000ff')
-      .setDescription(`${sender} is crying...`)
+      .setDescription(`${sender} sedang menangis...`)
       .setImage(cryGif);
 
     message.reply({ embeds: [embed] });
