27b15cbafd4675d417f8e10e4fc1761fa4798373
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-16 16:32:22 +0800

    Update thinking.js

--- AnimeCommands/thinking.js
+++ AnimeCommands/thinking.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'thinking',
-  description: 'Show that you\'re thinking!',
+  description: 'Ahli Fikir!',
   async execute(message, args) {
     const sender = message.author;
     const thinkingGif = await anime.thinking();
 
     const embed = new EmbedBuilder()
       .setColor('#99ccff')
-      .setDescription(`${sender} is thinking... 🤔`)
+      .setDescription(`${sender} sedang berfikir... 🤔`)
       .setImage(thinkingGif);
 
     message.reply({ embeds: [embed] });
