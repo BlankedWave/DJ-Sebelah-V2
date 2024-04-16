83a3a3bb1b0e9c22f78954e8f77e931795066d69
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-16 16:18:02 +0800

    Update hug.js

--- AnimeCommands/hug.js
+++ AnimeCommands/hug.js
@@ -3,7 +3,7 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'hug',
-  description: 'Send a hug gif in an embedded message!',
+  description: 'Bagi pelukan!',
   async execute(message, args) {
     
     const sender = message.author;
@@ -17,7 +17,7 @@ module.exports = {
  
     const embed = new EmbedBuilder()
       .setColor('#ff0000')
-      .setDescription(`${sender} sends a hug to ${targetUser}!`)
+      .setDescription(`${sender} bagi pelukan kepada ${targetUser}!`)
       .setImage(hugGif);
 
     
