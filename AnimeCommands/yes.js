51c8ddb7d101f677352f660dd297d6d0ea27473b
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-16 16:38:36 +0800

    Update yes.js

--- AnimeCommands/yes.js
+++ AnimeCommands/yes.js
@@ -3,14 +3,14 @@ const anime = require('anime-actions');
 const db = require("../mongodb");
 module.exports = {
   name: 'yes',
-  description: 'Express agreement or approval!',
+  description: 'Menyetujui akan perkara tersebut!',
   async execute(message, args) {
     const sender = message.author;
     const yesGif = await anime.yes();
 
     const embed = new EmbedBuilder()
       .setColor('#00cc66')
-      .setDescription(`${sender} says yes! 👍`)
+      .setDescription(`${sender} berkata iya! 👍`)
       .setImage(yesGif);
 
     message.reply({ embeds: [embed] });
