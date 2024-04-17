a711e63116d541c5bad9e10c8546666a689365ce
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-17 12:10:17 +0800

    Update ban.js

--- utilityCommands/ban.js
+++ utilityCommands/ban.js
@@ -9,42 +9,20 @@ module.exports = {
     description: 'Ban a user from the server',
     execute(message, args) {
         if (!message.member.permissions.has('BAN_MEMBERS')) {
-            return message.reply('❌ You do not have permission to use this command.');
+            return message.reply('❌ Anda tidak mempunyai hak untuk menggunakan perintah ini.');
         }
         const user = message.mentions.users.first();
         if (!user) {
-            return message.reply('❌ You need to mention a user to ban.');
+            return message.reply('❌ Anda perlu mention seorang pengguna untuk dilarang.');
         }
         const member = message.guild.members.cache.get(user.id);
         member.ban();
         const embed = new EmbedBuilder()
             .setColor('#FF0000')
-            .setTitle('User Banned ✅')
-            .setDescription(`▶️ ${user.tag} has been banned from the server by ${message.author.tag}.`)
+            .setTitle('Pengguna Diban ✅')
+            .setDescription(`▶️ ${user.tag} telah diban dari server oleh ${message.author.tag}.`)
             .setTimestamp();
 
         message.reply({ embeds: [embed] });
     },
 };
-
-
-/*
-
-   MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   FOR EMOJIS EITHER YOU CAN EDIT OR JOIN OUR DISCORD SERVER 
-   SO WE ADD BOT TO OUR SERVER SO YOU GET ANIMATED EMOJIS.
-
-   DISCORD SERVER : https://discord.gg/FUEHs7RCqz
-   YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-
-   FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/c4kaW2sSbm ]
-*/
