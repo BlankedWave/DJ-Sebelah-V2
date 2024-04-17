086e5b54799beacc040504bad749d1b1d79e4269
Muhammad Aqil <97868843+BlankedWave@users.noreply.github.com>
2024-04-17 12:07:53 +0800

    Update kick.js

--- utilityCommands/kick.js
+++ utilityCommands/kick.js
@@ -2,52 +2,30 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'kick',
-    description: 'Kick a user from the server',
+    description: 'Mengeluarkan seorang pengguna dari server',
     execute(message, args) {
-        // Check if the user has the necessary permissions to use the command
+        // Memeriksa jika pengguna mempunyai kebenaran yang diperlukan untuk menggunakan perintah ini
         if (!message.member.permissions.has('KICK_MEMBERS')) {
-            return message.reply('❌ You do not have permission to use this command.');
+            return message.reply('❌ Anda tidak mempunyai hak untuk menggunakan perintah ini.');
         }
 
-        // Check if a user was mentioned in the command
+        // Memeriksa jika seorang pengguna disebut dalam perintah
         const user = message.mentions.users.first();
         if (!user) {
-            return message.reply('❌ You need to mention a user to kick.');
+            return message.reply('❌ Anda perlu mention seorang pengguna untuk dikeluarkan.');
         }
 
-        // Kick the mentioned user
+        // Mengeluarkan pengguna yang disebut
         const member = message.guild.members.cache.get(user.id);
         member.kick();
 
-        // Create an embed object
+        // Mencipta objek embed
         const embed = new EmbedBuilder()
             .setColor('#FF0000')
-            .setTitle('User Kicked ✅')
-            .setDescription(`▶️ ${user.tag} has been kicked from the server by ${message.author.tag}.`)
+            .setTitle('Pengguna Dikeluarkan ✅')
+            .setDescription(`▶️ ${user.tag} telah dikeluarkan dari server oleh ${message.author.tag}.`)
             .setTimestamp();
 
         message.reply({ embeds: [embed] });
     },
 };
-
-
-/*
-
-   MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   FOR EMOJIS EITHER YOU CAN EDIT OR JOIN OUR DISCORD SERVER 
-   SO WE ADD BOT TO OUR SERVER SO YOU GET ANIMATED EMOJIS.
-
-   DISCORD SERVER : https://discord.gg/FUEHs7RCqz
-   YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-
-   FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/c4kaW2sSbm ]
-*/
